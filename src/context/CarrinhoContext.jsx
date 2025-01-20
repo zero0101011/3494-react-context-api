b67fba92d4f72151102c21b16ea2e53848fad9fc
Samuel Carlos <102079472+zero0101011@users.noreply.github.com>
2025-01-19 23:22:16 -0300

    .

---
+++ src/context/CarrinhoContext.jsx
@@ -0,0 +1,13 @@
+import React, { createContext } from "react";
+
+export const CarrinhoContext = createContext()
+
+export const CarrinhoProvider = ({ children }) =>  {
+
+    return (
+        <CarrinhoContext.Provider>
+            {children} 
+        </CarrinhoContext.Provider>>
+    )
+
+}
