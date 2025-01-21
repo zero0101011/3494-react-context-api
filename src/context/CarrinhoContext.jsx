e42ab497846f5fc1103a2b0a6e631beefabdf733
Samuel Carlos <102079472+zero0101011@users.noreply.github.com>
2025-01-21 02:02:06 -0300

    aula concluida

--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -3,13 +3,12 @@ import React, { createContext, useState } from "react";
 export const CarrinhoContext = createContext();
 
 export const CarrinhoProvider = ({ children }) =>  {
-
     const [carrinho, setCarrinho] = useState([]);
+    const [quantidade, setQuantidade] = useState(0);
 
     return (
-        <CarrinhoContext.Provider value={{ carrinho, setCarrinho }}>
+        <CarrinhoContext.Provider value={{ carrinho, setCarrinho, quantidade, setQuantidade }}>
             {children}
         </CarrinhoContext.Provider>
     );
-
 };
